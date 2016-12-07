56e6b0acb6afd833fb74eb2b0e9ed96a34fce0a5
Kumar Harsha <kumar.harsha@tum.de>
2016-12-07 20:27:22 +0100

    data using kknn on all genes

--- analysis-kknn.R
+++ analysis-kknn.R
@@ -3,3 +3,6 @@ source("common/generate-sample.R")
 source("kknn/kknn-functions.R")
 
 allGenes <- kknnImputeAllGenes()
+
+
+# ggplot(allGenes, aes(x=reorder(optimalK, -table(optimalK)[optimalK]))) + geom_bar() + coord_flip()
--- kknn/kknn-functions.R
+++ kknn/kknn-functions.R
@@ -1,11 +1,13 @@
 # function to compute results for a specific gene
 kknnImputeGene <- function(geneID) {
   trainingInfo <- train.kknn(as.formula(paste(geneID, ".", sep = "~")), simData.learn, kmax=40, kernel = c("triangular", "rectangular", "epanechnikov", "optimal"), distance = 2)
-  return(c(geneID, trainingInfo$best.parameters$k, trainingInfo$best.parameter$kernel))
+  r <- kknn(as.formula(paste(geneID, ".", sep = "~")), simData.learn, simData.test, distance = 2, kernel = trainingInfo$best.parameters$kernel, k = trainingInfo$best.parameters$k)
+  outputList <- list(optimalK = trainingInfo$best.parameters$k, optimalKernel = trainingInfo$best.parameter$kernel, predicted = r$fitted.values)
+  return(outputList)
 }
 
 # function to compute results for all genes
 kknnImputeAllGenes <- function() {
-  t <- apply(as.array(selectedGenes), 1, kknnImputeGene)
-  return(t)
+  genes <- mapply(kknnImputeGene, selectedGenes)
+  return(genes)
 }
