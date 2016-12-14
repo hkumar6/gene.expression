7128c9562712c1c8647a840e9bdcfd768b68c94b
Maria <ga67dus@mytum.de>
2016-12-14 14:33:17 +0100

    delete real and predicted value as output

--- lasso/lasso-functions.R
+++ lasso/lasso-functions.R
@@ -1,8 +1,8 @@
-LASSO <- function(geneID){
-  simData.gene.learn <- simData.learn[,-c(which(colnames(simData.learn) == geneID))]
-  simData.gene.test <- simData.test[,-c(which(colnames(simData.test) == geneID))]
-  gene.learn <- simData.learn[,which(colnames(simData.learn) == geneID)]
-  gene.test <- simData.test[,which(colnames(simData.test) == geneID)]
+LASSO <- function(ID){
+  simData.gene.learn <- simData.learn[,-c(which(colnames(simData.learn) == ID))]
+  simData.gene.test <- simData.test[,-c(which(colnames(simData.test) == ID))]
+  gene.learn <- simData.learn[,which(colnames(simData.learn) == ID)]
+  gene.test <- simData.test[,which(colnames(simData.test) == ID)]
   fit.expr <- glmnet(x = simData.gene.learn, y = gene.learn, family = "gaussian", standardize = TRUE)
   CV.expr <- cv.glmnet(simData.gene.learn,t(gene.learn), type.measure="mse")
   gene.predicted <- predict(fit.expr, newx = simData.gene.test, s = c(CV.expr$lambda.min))
@@ -10,8 +10,7 @@ LASSO <- function(geneID){
   c <- rcorr(gene.predicted,gene.test, type = "spearman")
   Spear_corr <- c$r[1,2]
   cov <- sum(coef(fit.expr, CV.expr$lambda.min)>0)
-  result <- list(mse = mse, Spear_corr = Spear_corr, real = gene.test, predicted = gene.predicted,
-                 number.cov = cov)
+  result <- list(mse = mse, Spear_corr = Spear_corr, number.cov = cov)
   return(result)
 }
 
