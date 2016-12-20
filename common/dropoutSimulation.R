84e9516a8729499ac4307ecf4a9b1894bb14e81e
Kumar Harsha <kumar.harsha@tum.de>
2016-12-20 17:37:12 +0100

    DropoutSimulation v2, functions to consider cells and genes dependency

--- common/dropoutSimulation.R
+++ common/dropoutSimulation.R
@@ -3,17 +3,18 @@
 DropoutSimulation <- setClass("DropoutSimulation",
                               slots = 
                                 c(
-                                  data.good = "ANY",
                                   dropout.percentage = "vector",
                                   n = "numeric",
-                                  simulation.result = "list"
+                                  simulation.result.genes = "list",
+                                  simulation.result.cells = "list"
                                 ),
                               prototype = list(
-                                simulation.result = list()
+                                simulation.result.genes = list(),
+                                simulation.result.cells = list()
                               ))
 
 
-# method to assign data for the gene expression
+# method to run simulations on genes with dependency on cells
 setGeneric(name = "simulateDropoutGene",
            def = function(theObject, expressionData, dropoutPercentage, n) {
              standardGeneric("simulateDropoutGene")
@@ -22,34 +23,75 @@ setGeneric(name = "simulateDropoutGene",
 setMethod(f = "simulateDropoutGene",
           signature = "DropoutSimulation",
           definition = function(theObject, expressionData, dropoutPercentage, n) {
-            theObject@data.good <- expressionData
             theObject@dropout.percentage <- dropoutPercentage
             theObject@n <- n
             
             # select 500 most abundantly expressed genes
-            local.means <- data.frame(rowMeans(theObject@data.good))
+            local.means <- data.frame(rowMeans(expressionData))
             names(local.means) <- c("rowMeans")
             rownames(local.means)[order(local.means$rowMeans, decreasing = TRUE)[1:500]] -> local.selectedGenes
             expr[local.selectedGenes, ] -> local.simData
             for (p in theObject@dropout.percentage) {
               for (simID in 1:n) {
-                sample(colnames(theObject@data.good), p*ncol(theObject@data.good)) -> local.selectedCells
+                sample(colnames(expressionData), p*ncol(expressionData)) -> local.selectedCells
                 
                 # simulation for lasso
-                td <- mapply(lassoImpute, local.selectedGenes[1:3],
+                td <- mapply(lassoImpute, local.selectedGenes,
                              MoreArgs = list(as.matrix(t(local.simData[, -which(colnames(local.simData) %in% local.selectedCells)])), 
                                              as.matrix(t(local.simData[, local.selectedCells]))))
-                theObject@simulation.result[[length(theObject@simulation.result)+1]] <- td
-                attr(theObject@simulation.result[[length(theObject@simulation.result)]], "drop-percentage") <- p
-                attr(theObject@simulation.result[[length(theObject@simulation.result)]], "method") <- "lasso"
+                theObject@simulation.result.genes[[length(theObject@simulation.result.genes)+1]] <- td
+                attr(theObject@simulation.result.genes[[length(theObject@simulation.result.genes)]], "drop-percentage") <- p
+                attr(theObject@simulation.result.genes[[length(theObject@simulation.result.genes)]], "method") <- "lasso"
                 
                 # simulation for kknn
-                td <- mapply(kknnImpute, local.selectedGenes[1:3],
+                td <- mapply(kknnImpute, local.selectedGenes,
                              MoreArgs = list(as.data.frame(t(local.simData[, -which(colnames(local.simData) %in% local.selectedCells)])),
                                              as.data.frame(t(local.simData[, local.selectedCells]))))
-                theObject@simulation.result[[length(theObject@simulation.result)+1]] <- td
-                attr(theObject@simulation.result[[length(theObject@simulation.result)]], "drop-percentage") <- p
-                attr(theObject@simulation.result[[length(theObject@simulation.result)]], "method") <- "kknn"
+                theObject@simulation.result.genes[[length(theObject@simulation.result.genes)+1]] <- td
+                attr(theObject@simulation.result.genes[[length(theObject@simulation.result.genes)]], "drop-percentage") <- p
+                attr(theObject@simulation.result.genes[[length(theObject@simulation.result.genes)]], "method") <- "kknn"
+              }
+            }
+            return(theObject)
+          })
+
+# method to run simulations on cells with dependency on genes
+setGeneric(name = "simulateDropoutCells",
+           def = function(theObject, expressionData, dropoutPercentage, n) {
+             standardGeneric("simulateDropoutCells")
+           })
+
+setMethod(f = "simulateDropoutCells",
+          signature = "DropoutSimulation",
+          definition = function(theObject, expressionData, dropoutPercentage, n) {
+            theObject@dropout.percentage <- dropoutPercentage
+            theObject@n <- n
+            
+            # select 500 most abundantly expressed genes
+            local.means <- data.frame(rowMeans(expressionData))
+            names(local.means) <- c("rowMeans")
+            rownames(local.means)[order(local.means$rowMeans, decreasing = TRUE)[1:500]] -> local.selectedGenes
+            expr[local.selectedGenes, ] -> local.simData
+            colnames(local.simData) <- gsub("^1", "X", colnames(local.simData), perl = TRUE)
+            colnames(local.simData) <- gsub("_", "", colnames(local.simData), perl = TRUE)
+            for (p in theObject@dropout.percentage) {
+              for (simID in 1:n) {
+                sample(rownames(local.simData), p*nrow(local.simData)) -> local.dropoutGenes
+                # simulation for lasso
+                td <- mapply(lassoImpute, colnames(local.simData),
+                             MoreArgs = list(as.matrix(local.simData[-which(rownames(local.simData) %in% local.dropoutGenes),]),
+                                             as.matrix(local.simData[local.dropoutGenes,]) ))
+                theObject@simulation.result.cells[[length(theObject@simulation.result.cells)+1]] <- td
+                attr(theObject@simulation.result.cells[[length(theObject@simulation.result.cells)]], "drop-percentage") <- p
+                attr(theObject@simulation.result.cells[[length(theObject@simulation.result.cells)]], "method") <- "lasso"
+                
+                # simulation for kknn
+                td <- mapply(kknnImpute, colnames(local.simData),
+                             MoreArgs = list(as.data.frame(local.simData[-which(rownames(local.simData) %in% local.dropoutGenes),]),
+                                             as.data.frame(local.simData[local.dropoutGenes,]) ))
+                theObject@simulation.result.cells[[length(theObject@simulation.result.cells)+1]] <- td
+                attr(theObject@simulation.result.cells[[length(theObject@simulation.result.cells)]], "drop-percentage") <- p
+                attr(theObject@simulation.result.cells[[length(theObject@simulation.result.cells)]], "method") <- "kknn"
               }
             }
             return(theObject)
--- kknn/kknn-functions.R
+++ kknn/kknn-functions.R
@@ -2,8 +2,7 @@
 kknnImpute <- function(id, simData.learn, simData.test) {
   trainingInfo <- train.kknn(as.formula(paste(id, ".", sep = "~")), simData.learn, kmax=40, kernel = c("triangular", "rectangular", "epanechnikov", "optimal"), distance = 2)
   r <- kknn(as.formula(paste(id, ".", sep = "~")), simData.learn, simData.test, distance = 2, kernel = trainingInfo$best.parameters$kernel, k = trainingInfo$best.parameters$k)
-  mse <- sum((simData.test[id]-r$fitted.values)^2)/(dim(simData.test[id])[1])
-  
+  mse <- sum((simData.test[,id]-r$fitted.values)^2)/(dim(simData.test[id])[1])
   x <- simData.test[id]
   x$predicted = r$fitted.values
   c <- rcorr(as.matrix(x), type = "spearman")
