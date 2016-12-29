d576028a87e6df8c05bae0914f9d8e85875e107d
Maria <ga67dus@mytum.de>
2016-12-29 16:47:44 +0100

    correction

--- mixedSpeciesLasso.R
+++ mixedSpeciesLasso.R
@@ -17,13 +17,14 @@ lasso.mixed.data <- function(ID,simData){
   vec.learn <- simData.learn[,which(colnames(simData.learn)==ID)]
   vec.test <- simData.test[,which(colnames(simData.test)==ID)]
   n = dim(simData_learn)[1]
+  n1 = length(vec.test)
   if (dim(simData_learn)[1]>1){
     fit.expr.lin <- glmnet(x = log(1+simData_learn), y = log(1+vec.learn),
                            family = "gaussian", standardize = TRUE, alpha = 1)
-  if (sum(simData_learn>0)/(dim(simData_learn)[1]*dim(simData_learn)[2]) > 0.5){
-    cv.expr.lin <- cv.glmnet(x = log(1+simData_learn), y = log(1+vec.learn), type.measure="mse")
+  if (n>10){
+    cv.expr.lin <- cv.glmnet(x = log(1+simData_learn), y = log(1+vec.learn), nfold=3, type.measure="mse")
     prediction <- predict(fit.expr.lin, newx = log(1+simData_test), s = c(cv.expr.lin$lambda.min))
-    cov <- sum(coef(fit.expr.lin, cv.expr.lin$lambda.min)>0)
+    cov <- sum(coef(fit.expr.lin, cv.expr.lin$lambda.min)!=0)
   } else {
     prediction.lin <- predict(fit.expr.lin, newx = log(1+simData_test))
     test <- apply(prediction.lin,2,function(x) mean((vec.test - (exp(x)-1))^2))
@@ -42,13 +43,14 @@ lasso.mixed.data <- function(ID,simData){
       cov = NaN
     }
   
-  result <- data.frame(mse = mse.lin, Spear_corr = Spear_corr, number.cov = cov, nonzero = n)
+  result <- data.frame(mse = mse.lin, Spear_corr = Spear_corr, number.cov = cov, nonzero = n,length.test = n1)
+  return(result)
 }
 
-ID <- rownames(simData)[700:710]
+ID <- colnames(simData)[700:710]
 
-result <- mapply(lasso.mixed.data,ID,MoreArgs = list(t(simData)))
-
+result <- mapply(lasso.mixed.data,ID,MoreArgs = list(simData))
+result
 
 
 
