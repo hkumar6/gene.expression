f3ed534ef79bd624df91b059593fb85517a17861
Maria <ga67dus@mytum.de>
2017-01-02 21:56:37 +0100

    Analysis for Imputing the mixed species data

--- mixedSpeciesAnalysis.R
+++ mixedSpeciesAnalysis.R
@@ -1,4 +1,4 @@
-#' The ImputationTest class
+#' The mixedSpeciesImput class
 #' 
 #' This class has slots to store results of different simulations
 #' using Lasso and kknn methods.
@@ -9,68 +9,68 @@
 #'  where cells are imputed as functions of genes
 #'  
 #' @importFrom methods new
-#' @exportClass ImputationTest
+#' @exportClass mixedSpeciesImput
 
-ImputationTest <- setClass("ImputationTest",
-                              slots = 
-                                c(
-                                  simulation.result.genes = "list",
-                                  simulation.result.cells = "list"
-                                ),
-                              prototype = list(
-                                simulation.result.genes = list(),
-                                simulation.result.cells = list()
-                              ))
+mixedSpeciesImput <- setClass("mixedSpeciesImput",
+                           slots = 
+                             c(
+                               simulation.result.genes = "list",
+                               simulation.result.cells = "list"
+                             ),
+                           prototype = list(
+                             simulation.result.genes = list(),
+                             simulation.result.cells = list()
+                           ))
 
 
 # method to run simulations on genes with dependency on cells
-setGeneric(name = "testImputationGene",
+setGeneric(name = "mixedSpeciesGene",
            def = function(theObject, mixedSpeciesdata) {
-             standardGeneric("testImputationGene")
+             standardGeneric("mixedSpeciesGene")
            })
 
-setMethod(f = "testImputationGene",
-          signature = "ImputationTest",
+setMethod(f = "mixedSpeciesGene",
+          signature = "mixedSpeciesImput",
           definition = function(theObject, mixedSpeciesdata) {
             
             # reduce randomly gene dimension by 50%
             sample(rownames(mixedSpeciesdata), 0.5*nrow(mixedSpeciesdata)) -> selectedGenes
             simData <- mixedSpeciesdata[selectedGenes,]
-
-                # simulation for lasso
-                td <- mapply(lasso.mixed.data, rownames(simData)[1:3],
-                             MoreArgs = list(t(simData)))
-                theObject@simulation.result.genes[[length(theObject@simulation.result.genes)+1]] <- td
-                attr(theObject@simulation.result.genes[[length(theObject@simulation.result.genes)]], "method") <- "lasso"
-                
-                # simulation for kknn
-                #TODO
-
+            
+            # simulation for lasso
+            td <- mapply(lasso.mixed.data, rownames(simData)[1:3],
+                         MoreArgs = list(t(simData)))
+            theObject@simulation.result.genes[[length(theObject@simulation.result.genes)+1]] <- td
+            attr(theObject@simulation.result.genes[[length(theObject@simulation.result.genes)]], "method") <- "lasso"
+            
+            # simulation for kknn
+            #TODO
+            
             return(theObject)
           })
 
 # method to run simulations on cells with dependency on genes
-setGeneric(name = "testImputationCells",
+setGeneric(name = "mixedSpeciesCells",
            def = function(theObject, mixedSpeciesdata) {
-             standardGeneric("testImputationCells")
+             standardGeneric("mixedSpeciesCells")
            })
 
-setMethod(f = "testImputationCells",
-          signature = "ImputationTest",
+setMethod(f = "mixedSpeciesCells",
+          signature = "mixedSpeciesImput",
           definition = function(theObject, mixedSpeciesdata) {
             
             # reduce randomly gene dimension by 50%
             sample(rownames(mixedSpeciesdata), 0.5*nrow(mixedSpeciesdata)) -> selectedGenes
             simData <- mixedSpeciesdata[selectedGenes,]
             selectedGenes <- rownames(simData)
-                # simulation for lasso
-                td <- mapply(lasso.mixed.data, colnames(simData)[1:3],
-                             MoreArgs = list(simData))
-                theObject@simulation.result.cells[[length(theObject@simulation.result.cells)+1]] <- td
-                attr(theObject@simulation.result.cells[[length(theObject@simulation.result.cells)]], "method") <- "lasso"
-                
-                # simulation for kknn
-                # TODO
-                
+            # simulation for lasso
+            td <- mapply(lasso.mixed.data, colnames(simData)[1:3],
+                         MoreArgs = list(simData))
+            theObject@simulation.result.cells[[length(theObject@simulation.result.cells)+1]] <- td
+            attr(theObject@simulation.result.cells[[length(theObject@simulation.result.cells)]], "method") <- "lasso"
+            
+            # simulation for kknn
+            # TODO
+            
             return(theObject)
           })
