973eed3d85b0803ddea521cc11968901d7c85caf
Siliegia <maria.mircea@yahoo.de>
2017-01-20 16:56:20 +0100

    added comparison for calculating right mse

--- R/mixedSpeciesLasso.R
+++ R/mixedSpeciesLasso.R
@@ -47,7 +47,26 @@ lasso.mixed.data <- function(ID,simData){
     cv.expr.lin <- cv.glmnet(x=simData_learn, y = log(1+vec.learn), nfold=3, type.measure="mse")
     prediction <- predict(fit.expr.lin, newx = simData_test, s = c(cv.expr.lin$lambda.min))
     prediction2 <- exp(prediction)-1
-    cov <- sum(coef(fit.expr.lin, cv.expr.lin$lambda.min)!=0)
+    coef <- sum(coef(fit.expr.lin, cv.expr.lin$lambda.min)!=0)
+    
+    if (length(grep("HUMAN",colnames(simData)[i]))>0){
+      test <- colnames(simData)[which(coef(fit.expr.lin, cv.expr.lin$lambda.min)!=0)]
+      H.coef <- grep("HUMAN",test)
+      coef.perc <- length(H.coef)/coef
+      M.test <- grep("MOUSE",rownames(simData_test))
+      prediction.new <- prediction[M.test]
+      counts <- sum(prediction.new = 0)
+      mse.bin <- mean(prediction.new)
+    }else{
+      test <- colnames(simData)[which(coef(fit.expr.lin, cv.expr.lin$lambda.min)!=0)]
+      H.coef <- grep("HUMAN",test)
+      coef.perc <- length(H.coef)/coef
+      H.test <- grep("HUMAN",rownames(simData_test))
+      prediction.new <- prediction[H.test,drop=FALSE]
+      counts <- sum(prediction.new = 0)
+      mse.bin <- mean(prediction.new)
+    }
+    
   } else { # if there are less than 9 non zero entries compare results with mse
     prediction.lin <- predict(fit.expr.lin, newx = simData_test)
     test <- apply(prediction.lin,2,function(x) mean(((exp(x)-1))^2))
@@ -56,16 +75,36 @@ lasso.mixed.data <- function(ID,simData){
       cov = NaN
     } else {
       j  <- which(test == min(test))[1]
-    cov <- sum(coef(fit.expr.lin, s = fit.expr.lin$lambda[j])!=0) }}
+    coef <- sum(coef(fit.expr.lin, s = fit.expr.lin$lambda[j])!=0) }}
+    
+    if (length(grep("HUMAN",colnames(simData)[i]))>0){
+      test <- colnames(simData)[which(coef(fit.expr.lin, s = fit.expr.lin$lambda[j])!=0)]
+      H.coef <- grep("HUMAN",test)
+      coef.perc <- length(H.coef)/coef
+      M.test <- grep("MOUSE",rownames(simData_test))
+      prediction.new <- prediction[M.test]
+      counts <- sum(prediction.new = 0)
+      mse.bin <- mean(prediction.new)
+    }else{
+      test <- colnames(simData)[which(coef(fit.expr.lin, s = fit.expr.lin$lambda[j])!=0)]
+      H.coef <- grep("HUMAN",test)
+      coef.perc <- length(H.coef)/coef
+      H.test <- grep("HUMAN",rownames(simData_test))
+      prediction.new <- prediction[H.test,drop=FALSE]
+      counts <- sum(prediction.new = 0)
+      mse.bin <- mean(prediction.new)
+    }
   
   mse.lin <- mean(((exp(prediction)-1))^2)
   }else{
     mse.lin = NaN
-    Spear_corr = NaN
-    cov = NaN
+    coef = NaN
+    coef.perc = NaN
+    counts = NaN
   }
   
-  result <- data.frame(mse = mse.lin, number.cov = cov, nonzero = n)
+  result <- data.frame(mse = mse.bin, number.coef = coef, nonzero = n, perc.human = coef.perc,
+                       zero.predicted = counts)
   return(result)
 }
 
---
+++ R/mixedSpeciesLassoNegbin.R
@@ -0,0 +1,51 @@
+
+lasso.negbin.mixed.data <- function(ID,simData){
+  i = which(colnames(simData)==ID)
+  
+  #formate for lasso function
+  simData <- as.data.frame((simData)) 
+  
+  #define test and learn data
+  simData.test <- simData[which(simData[,i]==0),,drop=FALSE] # define all zero entries of ID as test
+  simData.learn <- simData[-c(which(simData[,i]==0)),,drop=FALSE] # define all non-zero entries as learn
+  
+  # take out ID from the data set for analysis
+  simData_learn <- simData.learn[,-c(which(colnames(simData.learn)==ID)),drop = FALSE] 
+  simData_test <- simData.test[,-c(which(colnames(simData.test)==ID)),drop = FALSE]
+  
+  #define the vector ID for analysis
+  vec.learn <- simData.learn[,which(colnames(simData.learn)==ID)]
+  vec.test <- simData.test[,which(colnames(simData.test)==ID)]
+  n = dim(simData_learn)[1] # number of non-zero entries
+  if (dim(simData_learn)[1]>2 && dim(simData_test)[1]>2){ #if there is more than one entry non zero do regression
+    fit.expr.bin <- glmreg(vec.learn ~ ., data = simData_learn, family = "negbin",  theta=1)
+    prediction <- predict(fit.expr.bin, newx = simData_test, which = 30, type = "response")
+    
+    coef = sum(coef(fit.expr.bin, which = 30) !=0)
+    if (length(grep("HUMAN",colnames(simData)[i]))>0){
+      test <- colnames(simData)[which(coef(fit.expr.bin, which = 30)!=0)]
+      H.coef <- grep("HUMAN",test)
+      coef.perc <- length(H.coef)/coef
+      M.test <- grep("MOUSE",rownames(simData_test))
+      prediction.new <- prediction[M.test]
+      counts <- sum(prediction.new = 0)
+      mse.bin <- mean(prediction.new)
+    }else{
+      test <- colnames(simData)[which(coef(fit.expr.bin, which = 30)!=0)]
+      H.coef <- grep("HUMAN",test)
+      coef.perc <- length(H.coef)/coef
+      H.test <- grep("HUMAN",rownames(simData_test))
+      prediction.new <- prediction[H.test,drop=FALSE]
+      counts <- sum(prediction.new = 0)
+      mse.bin <- mean(prediction.new)
+    }
+    
+  }else{
+    mse.bin = NaN
+    coef = NaN
+    coef.perc = NaN
+    counts = NaN
+  }
+                       zero.predicted = counts)
+  return(result)
+}
