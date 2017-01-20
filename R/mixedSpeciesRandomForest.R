77e968d12ada4455a50d6e4722f04d8f11442a81
pimoloco <simonefurcas82@gmail.com>
2017-01-20 19:21:26 +0100

    new function mixed species
    by me coping by you both

---
+++ R/mixedSpeciesRandomForest.R
@@ -0,0 +1,46 @@
+randomForest.mixed.data <- function(id,simData){
+
+    
+  i = which(colnames(simData)==ID)
+  simData.test <- simData[which(simData[,i]==0),,drop=FALSE] # define all zero entries of ID as test
+  simData.learn <- simData[-c(which(simData[,i]==0)),,drop=FALSE] # define all non-zero entries as learn    
+  # define all zero entries of ID as test
+ # simData.test <- simData[which(simData[,id]==0),,drop=FALSE]
+  # define all non-zero entries as learn
+#  simData.learn <- simData[-c(which(simData[,id]==0)),,drop=FALSE]
+  
+  
+  
+  # define all zero entries of ID as test
+ # simData.test <- simData[which(simData[,id]==0),,drop=FALSE]
+  # define all non-zero entries as learn
+#  simData.learn <- simData[-c(which(simData[,id]==0)),,drop=FALSE]
+  
+  if (length(grep("HUMAN",colnames(simData)[i]))>0){
+    
+    M.test <- grep("MOUSE",rownames(prediction))
+    prediction.new <- prediction[M.test]
+    counts <- sum(prediction.new == 0)
+    mse.lin <- mean(prediction.new)
+  }else{
+    H.test <- grep("HUMAN",rownames(prediction))
+    prediction.new <- prediction[H.test,drop=FALSE]
+    counts <- sum(prediction.new == 0)
+    mse.lin <- mean(prediction.new)
+  }
+  
+  
+  
+  if(nrow(simData.learn) > 3) {
+    kmax <- sum(apply(simData.learn, 2, sum) != 0)
+    kmax <- if(kmax > 20) 20 else kmax
+    output <- randomForestImpute(id, simData.learn = as.data.frame(simData.learn), simData.test = as.data.frame(simData.test), kmax)
+    # output <- estimateKmax(id, simData.learn = simData_learn, simData.test = simData_test, kmax)
+    return(output)
+  } else {
+    noImputationOutput <- data.frame(mse = NA, Spear_corr = NA,
+                                     optimalK = NA,
+                                     optimalKernel = NA)
+    return(noImputationOutput)
+  }
+}
