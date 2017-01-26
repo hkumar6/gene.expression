2c8217901c6c1b73063ca53d58009298d3213a2a
pimoloco <simonefurcas82@gmail.com>
2017-01-26 15:25:08 +0100

    some changes

--- R/mixedSpeciesRandomForest.R
+++ R/mixedSpeciesRandomForest.R
@@ -28,13 +28,13 @@ randomForest.mixed.data <- function(id,simData){
          # counts <- sum(prediction.new == 0)
           #mse.lin <- mean(prediction.new)
         #}
+  #define the vector ID for analysis
   
   vec.learn <- simData.learn[,which(colnames(simData.learn)==id)]
   vec.test <- simData.test[,which(colnames(simData.test)==id)]
   n = dim(simData_learn)[1] # number of non-zero entries
   if (dim(simData_learn)[1]>2){ #if there is more than one entry non zero do regression
-    fit.expr.lin <- glmnet(x = simData_learn, y = log(1+vec.learn),
-                           family = "gaussian", standardize = TRUE, alpha = 1)
+    fit.expr.lin <- randomForest(x = simData_learn, y = log(1+vec.learn))
     
     if (n>=12){ # if there are more than 9 non zero entries cross-validation is possible
       cv.expr.lin <- cv.glmnet(x=simData_learn, y = log(1+vec.learn), nfold=3, type.measure="mse")
@@ -67,12 +67,12 @@ randomForest.mixed.data <- function(id,simData){
   
   
   
-  if(nrow(simData.learn) > 3) {
-    output <- randomForestImpute(id, simData.learn, simData.test)
-    # output <- estimateKmax(id, simData.learn = simData_learn, simData.test = simData_test, kmax)
-    return(output)
-  } else {
-    noImputationOutput <- data.frame(mse = NA, Spear_corr = NA)
-    return(noImputationOutput)
-  }
-}
+#   if(nrow(simData.learn) > 3) {
+#     output <- randomForestImpute(id, simData.learn, simData.test)
+#     # output <- estimateKmax(id, simData.learn = simData_learn, simData.test = simData_test, kmax)
+#     return(output)
+#   } else {
+#     noImputationOutput <- data.frame(mse = NA, Spear_corr = NA)
+#     return(noImputationOutput)
+#   }
+# }
